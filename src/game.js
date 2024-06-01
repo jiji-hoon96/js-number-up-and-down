aa1e9072bb580ed306a507c921eb9607c9eb8c98
JIHOON LEE <jihoon7705@gmail.com>
2024-06-01 17:22:47 +0900

    fix : 정답 출력 방식 오류 수정

--- src/game.js
+++ src/game.js
@@ -1,62 +1,66 @@
-const minNum = Number(sessionStorage.getItem('minNum'));
-const maxNum = Number(sessionStorage.getItem('maxNum'));
-const maxAttemptNum = Number(sessionStorage.getItem('maxAttemptNum'));
+const minNum = Number(sessionStorage.getItem("minNum"));
+const maxNum = Number(sessionStorage.getItem("maxNum"));
+const maxAttemptNum = Number(sessionStorage.getItem("maxAttemptNum"));
 let correctNum = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 let attemptNum = 0;
 
 const createLogElement = (text, className) => {
-  const logElement = document.createElement('li');
+  const logElement = document.createElement("li");
   logElement.textContent = text;
   logElement.classList.add(className);
   return logElement;
 };
 
 const appendLogElements = (elements) => {
-  const progressArea = document.querySelector('.progress-log');
-  elements.forEach(element => progressArea.appendChild(element));
+  const progressArea = document.querySelector(".progress-log");
+  elements.forEach((element) => progressArea.appendChild(element));
 };
 
 const showResult = () => {
-  const progressArea= document.querySelector('.progress-log');
-  const resultLog = document.createElement('li');
-  resultLog.textContent = `[컴퓨터] 축하합니다! 정답을 ${attemptNum}회만에 숫자를 맞추셨습니다.`;
-  resultLog.classList.add('computer-log');
-  progressArea.appendChild(resultLog);  
-}
+  const progressArea = document.querySelector(".progress-log");
+  const resultLog = document.createElement("li");
+  resultLog.textContent = `[컴퓨터] 축하합니다! 정답을 ${attemptNum}회만에 숫자를 맞추셨습니다. 정답은 ${correctNum}입니다!`;
+  resultLog.classList.add("computer-log");
+  progressArea.appendChild(resultLog);
+};
 
 const guessNumber = () => {
-  const input = document.getElementById('guess-input');
+  const input = document.getElementById("guess-input");
   const inputNum = Number(input.value);
-  const userLog = createLogElement(`[유저] : ${inputNum}`, 'user-log');
+  const userLog = createLogElement(`[유저] : ${inputNum}`, "user-log");
   let computerLog;
-  input.value =''
+  input.value = "";
 
   attemptNum++;
 
-  if (attemptNum === maxAttemptNum) {
-    const confirmGameBtn = document.getElementById('submit-guess');
-    computerLog = createLogElement(`[컴퓨터] : 게임 오버, 정답은 ${correctNum}입니다.`, 'computer-log');
-    confirmGameBtn.disabled = true;
-    input.disabled = true;
-    confirmGameBtn.style.backgroundColor = 'grey';
-    appendLogElements([userLog, computerLog]);
-    return;
-  }
-
   if (inputNum === correctNum) {
-    computerLog = createLogElement("[컴퓨터] : 정답!", 'computer-log');
-    appendLogElements([userLog, computerLog]);
     showResult();
     return;
   }
 
-  computerLog = createLogElement(inputNum < correctNum ? "[컴퓨터] : 업" : "[컴퓨터] : 다운", 'computer-log');
+  if (attemptNum === maxAttemptNum) {
+    const confirmGameBtn = document.getElementById("submit-guess");
+    computerLog = createLogElement(
+      `[컴퓨터] : 게임 오버, 정답은 ${correctNum}입니다.`,
+      "computer-log"
+    );
+    confirmGameBtn.disabled = true;
+    input.disabled = true;
+    confirmGameBtn.style.backgroundColor = "grey";
+    appendLogElements([userLog, computerLog]);
+    return;
+  }
+
+  computerLog = createLogElement(
+    inputNum < correctNum ? "[컴퓨터] : 업" : "[컴퓨터] : 다운",
+    "computer-log"
+  );
   appendLogElements([userLog, computerLog]);
 };
 
 const goToM = () => {
-  window.location.href = 'main.html';
+  window.location.href = "main.html";
 };
 
-document.getElementById('submit-guess').addEventListener('click', guessNumber);
-document.getElementById('go-to-main').addEventListener('click',goToM )
+document.getElementById("submit-guess").addEventListener("click", guessNumber);
+document.getElementById("go-to-main").addEventListener("click", goToM);
